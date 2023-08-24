e5362461757d047d682075c35e4cedae70b48296
Dariusz Sułtanowski <sultanowski.d@gmail.com>
2023-08-24 13:21:01 +0000

    dev: get data from Google Spreadsheets API (2)

--- counter.js
+++ counter.js
@@ -1,19 +1,13 @@
 import dotenv from 'dotenv';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
 dotenv.config();
 
-console.log(process.env.SHEET_ID);
-console.log(process.env.GOOGLE_SHEETS_API);
-console.log(process.env);
+const doc = new GoogleSpreadsheet(process.env.SHEET_ID, { apiKey: process.env.GOOGLE_SHEETS_API });
 
-// import { GoogleSpreadsheet } from 'google-spreadsheet';
+await doc.loadInfo();
+console.log(doc.title);
 
-// const doc = new GoogleSpreadsheet(process.env.GSID, { apiKey: process.env.GSAPI });
-// const doc = new GoogleSpreadsheet(process.env.SHEET_ID, { apiKey: process.env.GOOGLE_SHEETS_API });
-
-// await doc.loadInfo(); // loads document properties and worksheets
-// console.log(doc.title);
-
-// const sheet = doc.sheetsByIndex[1]; // or use `doc.sheetsById[id]` or `doc.sheetsByTitle[title]`
-// await sheet.loadCells('A1');
-// console.log(sheet.title);
-// console.log(sheet.getCell(0, 0).value);
+const sheet = doc.sheetsByIndex[1];
+await sheet.loadCells('A1');
+console.log(sheet.title);
+console.log(sheet.getCell(0, 0).value);
